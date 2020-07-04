fa6a03b355012a059977561868e7516ca7004370
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-07-04 08:34:24 +0000

    added header

---
+++ public/modules/header/header.js
@@ -0,0 +1,65 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+
+    this.template = `
+       <div class="text"> 
+          <div> Careers  </div>
+          <div> at Boldare  </div>
+       </div>
+        <div class="first-image"> </div>
+        <div class="second-image"> </div>
+        </span>
+        `;
+  }
+
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+
+    // var att = this.getAttribute("data"); // -> "Hello World"
+
+    // // console.log(JSON.parse(encodeURIComponent('{}')));
+    // let obj = JSON.parse(att);
+    // console.log(att);
+
+    // console.log(obj.name);
+
+    // list.setAttribute("data", "A new value");
+    // list.getAttribute("data"); // -> "A new value"
+    let container = document.createElement("div");
+    container.classList.add("header-container");
+    this.shadowRoot.appendChild(container);
+
+    //append HTML Template to container
+    container.innerHTML = this.template;
+    //Load Stylesheet this way
+    var preloadLink = document.createElement("link");
+    preloadLink.href = "/modules/header/header.css";
+    preloadLink.rel = "stylesheet";
+    this.shadowRoot.appendChild(preloadLink);
+
+    //create a handler for description
+    let des = `Case Study`;
+
+    let theme = this.getAttribute("theme"); // -> "A new value"
+    if (theme === "dark") {
+      console.log("Theme Dark");
+      container.classList.add("dark");
+    } else {
+      console.log("Theme White");
+      container.classList.add("white");
+    }
+  }
+
+  disconnectedCallback() {}
+
+  static get observedAttributes() {
+    return ["data", "theme"];
+  }
+  attributeChangedCallback(attr, oldValue, newValue) {
+    // if (attr === 'foo') this.doSomething();
+    // console.log("Attribute Changed");
+  }
+}
+
+window.customElements.define("pip-header", Header);
