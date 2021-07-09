020a3f490d4365024d6ffc4f1366b46f320803f9
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2021-07-09 07:18:50 +0000

    added a chat

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var fs = require("fs");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
